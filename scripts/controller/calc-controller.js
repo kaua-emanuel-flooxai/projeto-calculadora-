50421ae2f1b3e54e3beb546cca6858c9b6bbc931
WasabyDev <kepoxander@gmail.com>
2025-05-21 13:00:28 -0300

    feat: adding operators and numbers and adding click events

--- scripts/controller/calc-controller.js
+++ scripts/controller/calc-controller.js
@@ -1,11 +1,13 @@
 class Calccontroller {
   constructor() {
+    this._operation = [];
     this._locale = "pt-BR";
     this._displayCalcEl = document.querySelector("#display");
     this._dateCalcEl = document.querySelector("#data");
     this._timeCalcEl = document.querySelector("#hora");
     this._currentDate;
     this.initialize();
+    this.initButtonsEvents();
   }
 
   initialize() {
@@ -14,6 +16,80 @@ class Calccontroller {
     setInterval(() => {
       this.setDisplayDateTime();
     }, 1000);
+  }
+
+  addEventListenerAll(element, events, fn) {
+    events.split(" ").forEach((event) => {
+      element.addEventListener(event, fn, false);
+    });
+  }
+
+  initButtonsEvents() {
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+    buttons.forEach((btn, index) => {
+      this.addEventListenerAll(btn, "click drag", (e) => {
+        console.log(btn.className.baseVal.replace("btn-", ""));
+      });
+
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", (e) => {
+        btn.style.cursor = "pointer";
+      });
+    });
+  }
+
+  clearAll() {
+    this._oparation = [];
+  }
+
+  crearEntry() {
+    this._operation.pop();
+  }
+
+  addOparation(value) {
+    this._operation.push(value);
+  }
+
+  setError() {
+    this.displayCalc = "error";
+  }
+
+  execBtn(value) {
+    switch (value) {
+      case "ac":
+        this.clearAll();
+        break;
+      case "ce":
+        this.crearEntry();
+        break;
+      case "soma":
+        break;
+      case "subtracao":
+        break;
+      case "divisao":
+        break;
+      case "multiplicacao":
+        break;
+      case "porcento":
+        break;
+      case "igual":
+        break;
+
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+        break;
+
+      default:
+        this.setError();
+    }
   }
 
   setDisplayDateTime() {
